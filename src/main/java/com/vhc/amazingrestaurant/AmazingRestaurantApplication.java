294c57e4d5e510d3f68f367b7e69584ac85b722a
tanthanhvu <tan.vt195913@sis.hust.edu.vn>
2024-02-27 09:26:33 +0700

    first commit

---
+++ src/main/java/com/vhc/amazingrestaurant/AmazingRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.vhc.amazingrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazingRestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vhc/amazingrestaurant/AmazingRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhc.amazingrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
